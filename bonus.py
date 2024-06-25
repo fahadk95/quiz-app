61cd4dcac56a7cf245e890e377b512066c870a46
Fahad Mohd Khan <fahad.postbox@gmail.com>
2024-06-25 16:38:10 +0530

    quiz-app

---
+++ bonus.py
@@ -0,0 +1,13 @@
+import json
+
+with open("questions.json", 'r') as file:
+    content = file.read()
+
+data = json.loads(content)
+
+for question in data:
+    print(question["question_text"])
+    for index, alternative in enumerate(question["alternatives"]):
+        print(index + 1, "-", alternative)
+    user_choice = int(input("Enter your answer"))
+    
